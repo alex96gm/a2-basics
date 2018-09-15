3a02b16e7497a194c670bc16fa4165aae37313da
Paula Quitián <pqhernandez@gmail.com>
2018-09-15 22:30:47 +0200

    showing errors

--- a2-binding/src/app/components/films/film-form/film-form.component.ts
+++ a2-binding/src/app/components/films/film-form/film-form.component.ts
@@ -4,14 +4,16 @@ import { Film } from '../../../shared/models/film.model';
 @Component({
   selector: 'app-film-form',
   templateUrl: './film-form.component.html',
+  styleUrls: ['./film-form.component.css']
 })
 export class FilmFormComponent  {
   newFilm: Film = new Film();
 
   @Output() createFilm: EventEmitter<Film> = new EventEmitter<Film>();
   
-
   onCreateFilm() {
     this.createFilm.emit(this.newFilm);
+    this.newFilm = new Film();
   }
 }
+
